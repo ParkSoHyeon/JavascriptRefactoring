e374aef80bada15448994ca4536395fa000eb053
ParkSoHyeon <dso753@naver.com>
2021-03-09 22:26:02 +0900

    Chapter_01 단계 쪼개기(중간 데이터 구조 - 총합 계산, 함수 옮기기)

--- Chapter_01/index.js
+++ Chapter_01/index.js
@@ -5,6 +5,8 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -52,6 +54,24 @@ function statement(invoice, plays) {
 
         return volumeCredits;
     }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -61,28 +81,9 @@ function renderPlainText(data, plays) {
         result += `    ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
 
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
-
-    // 중첩 함수 시작(계산 로직을 여러 개의 보조 함수로 추출)
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-
-        return result;
-    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat(
