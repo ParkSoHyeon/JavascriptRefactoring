18c4c7dce7f71ae5e9d9d0e4929e574d9f74441d
ParkSoHyeon <dso753@naver.com>
2021-03-14 23:01:53 +0900

    Chapter_01 다형성 활용(조건부 로직을 다형성으로 바꾸기 - 적립포인트 계산)

--- Chapter_01/createStatementData.js
+++ Chapter_01/createStatementData.js
@@ -9,13 +9,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(this.performance.audience - 30, 0);
-
-        if ('comedy' === this.play.type)
-            volumeCredits += Math.floor(this.performance.audience / 5);
-
-        return volumeCredits;
+        return Math.max(this.performance.audience - 30, 0);
     }
 
 }
@@ -38,6 +32,10 @@ class ComedyCalculator extends PerformanceCalculator {
         }
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
