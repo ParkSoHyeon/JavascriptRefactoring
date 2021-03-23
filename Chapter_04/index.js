2595d348311cbf0eafed2c1d1ce03da2c8cabf59
ParkSoHyeon <dso753@naver.com>
2021-03-23 20:18:14 +0900

    Chapter_04 sampleProvinceData 함수 생성

--- Chapter_04/index.js
+++ Chapter_04/index.js
@@ -52,3 +52,16 @@ class Province {
         this._totoalProduction += arg.production;
     }
 }
+
+function sampleProvinceData() {
+    return {
+        name: 'Asia',
+        producers: [
+            { name: "Byzantium", cost: 10, production: 9 },
+            { name: "Attalia", cost: 12, production: 10 },
+            { name: "Sinope", cost: 10, production: 6 },
+        ],
+        demand: 30,
+        price: 20
+    }
+}
