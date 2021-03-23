cb5a1a416632c755c04e7e4a6245d4097dea919c
ParkSoHyeon <dso753@naver.com>
2021-03-24 00:28:16 +0900

    Chapter_06 예시: 지역 변수의 값을 변경할 때 => 추출할 부분을 새로운 함수로 복사

--- Chapter_06/index.js
+++ Chapter_06/index.js
@@ -20,6 +20,15 @@ function printOwing(invoice) {
     }
 }
 
+function calculateOutstanding(invoice) {
+    let outstanding = 0;
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    return outstanding;
+}
+
 function recordDueDate(invoice) {
     const today = new Date();
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
