aae06088311f74910ce7b5c223fe32d86f3130d8
ParkSoHyeon <dso753@naver.com>
2021-03-23 20:45:11 +0900

    Chapter_04 Producer 클래스 생성

--- Chapter_04/index.js
+++ Chapter_04/index.js
@@ -1,8 +1,33 @@
 class Producer {
-    constructor(province, d) {
-
+    constructor(aProvince, data) {
+        this._province = aProvince;
+        this._cost = data.cost;
+        this._name = data.name;
+        this._production = data.production || 0;
     }
 
+    get name() {
+        return this._name;
+    }
+
+    get cost() {
+        return this._name;
+    }
+
+    set name(arg) {
+        this._cost = parseInt(arg);
+    }
+
+    get production() {
+        return this._production;
+    }
+
+    set production(amountStr) {
+        const amount = parseInt(amountStr);
+        const newProduction = Number.isNaN(amount) ? 0 : amount;
+        this._province.totalProduction += newProduction - this._production;
+        this._production = newProduction;
+    }
 }
 
 class Province {
