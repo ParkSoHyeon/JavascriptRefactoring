a245ff6f4cbcdd492ed840979b211610e8414ff8
ParkSoHyeon <dso753@naver.com>
2021-03-24 00:22:56 +0900

    Chapter_06 예시: 지역 변수를 사용할 때 => 마감일 설정 로직을 함수로 추출

--- Chapter_06/index.js
+++ Chapter_06/index.js
@@ -9,20 +9,21 @@ function printOwing(invoice) {
     }
 
     // 마감일(dueDate)을 기록한다.
-    const today = new Date();
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    recordDueDate(invoice);
 
     // 세부 사항을 출력한다.
     printDetails(invoice, outstanding);
-
 
     function printBanner() {
         console.log('*******************');
         console.log('**** 고객 채무 ****');
         console.log('*******************');
     }
+}
 
-
+function recordDueDate(invoice) {
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
 function printDetails(invoice, outstanding) {
