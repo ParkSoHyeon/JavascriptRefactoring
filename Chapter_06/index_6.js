07d206cb84c4b35b4ff581a34d69800202a8a91b
ParkSoHyeon <dso753@naver.com>
2021-03-25 00:39:19 +0900

    Chapter_06 예시: 변수 캡슐화하기 => 세터 함수 호출하도록 수정

--- Chapter_06/index_6.js
+++ Chapter_06/index_6.js
@@ -2,7 +2,7 @@ let defaultOwner = {firstName: '마틴', lastName: '파울러'};
 
 spaceship.owner = getDefaultOwner();
 
-defaultOwner = {firstName: '레베카', lastName: '파슨스'};
+setDefaultOwner({firstName: '레베카', lastName: '파슨스'});
 
 function getDefaultOwner() {
     return defaultOwner;
