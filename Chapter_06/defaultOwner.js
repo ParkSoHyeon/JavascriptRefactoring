2e3c82d7d3f4433608b5036bfd1291c467ec1d54
ParkSoHyeon <dso753@naver.com>
2021-03-25 01:44:07 +0900

    Chapter_06 예시: 변수 캡슐화하기 => 값 캡슐화하기(레코드 캡슐화하기)

--- Chapter_06/defaultOwner.js
+++ Chapter_06/defaultOwner.js
@@ -1,11 +1,26 @@
 let defaultOwnerData = {firstName: '마틴', lastName: '파울러'};
 
 function defaultOwner() {
-    return  Object.assign({}, defaultOwnerData);
+    return new Person(defaultOwnerData);
 }
 
 function setDefaultOwner(arg) {
     defaultOwnerData = arg;
+}
+
+class Person {
+    constructor(data) {
+        this._lastName = data.lastName;
+        this._firstName = data.firstName;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
 }
 
 module.exports = { defaultOwner, setDefaultOwner };
