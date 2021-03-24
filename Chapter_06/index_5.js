08ea9fbe1f301f8ede91770fac2e767e72532035
ParkSoHyeon <dso753@naver.com>
2021-03-25 00:15:20 +0900

    Chapter_06 예시: 매개변수 추가하기 => 매개변수 추가

--- Chapter_06/index_5.js
+++ Chapter_06/index_5.js
@@ -1,9 +1,9 @@
 class Book {
     addReservation(customer) {
-        zz_addReservation(customer);
+        zz_addReservation(customer, false);
     }
 
-    zz_addReservation(customer) {
+    zz_addReservation(customer, isPriority) {
         this._reservations.push(customer);
     }
 }
