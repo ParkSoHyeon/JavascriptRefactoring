8f0d9371c8fb84fd741199e3c13ead6afbf4490a
ParkSoHyeon <dso753@naver.com>
2021-03-25 00:24:30 +0900

    Chapter_06 예시 기본 코드: 매개변수를 속성으로 바꾸기 => 함수 추출하기로 새함수 생성

--- Chapter_06/index_5.js
+++ Chapter_06/index_5.js
@@ -1,6 +1,11 @@
 function inNewEngland(aCustomer) {
     const stateCode = aCustomer.address.state;
+    return xxNEWinNewEngland(stateCode);
+}
+
+function xxNEWinNewEngland(stateCode) {
     return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
+
 
 const newEnglanders = someCustomers.filter(c => inNewEngland(c));
