f3612931f564c00aebc568a4e4520910e1386e7f
ParkSoHyeon <dso753@naver.com>
2021-03-25 00:38:47 +0900

    Chapter_06 예시: 변수 캡슐화하기 => 게터 함수 호출하도록 수정

--- Chapter_06/index_6.js
+++ Chapter_06/index_6.js
@@ -1,6 +1,6 @@
 let defaultOwner = {firstName: '마틴', lastName: '파울러'};
 
-spaceship.owner = defaultOwner;
+spaceship.owner = getDefaultOwner();
 
 defaultOwner = {firstName: '레베카', lastName: '파슨스'};
 
