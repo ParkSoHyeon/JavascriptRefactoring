4a2cc1a01ed7267a04308963a38faeba97bcf9da
ParkSoHyeon <dso753@naver.com>
2021-03-25 00:17:23 +0900

    Chapter_06 예시: 매개변수 추가하기 => 기존 함수 인라인 및 새 함수의 이름 기존 함수의 이름으로 변경

--- Chapter_06/index_5.js
+++ Chapter_06/index_5.js
@@ -1,9 +1,5 @@
 class Book {
     addReservation(customer) {
-        zz_addReservation(customer, false);
-    }
-
-    zz_addReservation(customer, isPriority) {
         assert(isPriority === true || isPriority === false);
         this._reservations.push(customer);
     }
