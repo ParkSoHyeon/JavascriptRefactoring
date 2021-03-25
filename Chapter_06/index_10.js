7386861c837e8630f27430fe88aafb53321de383
ParkSoHyeon <dso753@naver.com>
2021-03-26 01:08:26 +0900

    Chapter_06 예시: 여러 함수를 변환 함수로 묶기 => 변환 함수 생성 및 사용

--- Chapter_06/index_10.js
+++ Chapter_06/index_10.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 reading = {customer: 'ivan', quantity: 10, month: 5, year: 2017};
 
 // 클라이언트 1
@@ -11,11 +13,17 @@ const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 
 // 클라이언트 3
-const aReading = acquireReading();
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
 const basicChargeAmount = calculateBaseCharge(aReading);
 
 function calculateBaseCharge(aReading) {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+function enrichReading(original) {
+    const result = _.cloneDeep(original);
+    return result;
 }
 
 
