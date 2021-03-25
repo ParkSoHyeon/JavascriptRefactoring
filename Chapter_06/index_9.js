5879e829af60c9d50acae80de860620946dc0314
ParkSoHyeon <dso753@naver.com>
2021-03-26 00:51:30 +0900

    Chapter_06 예시: 여러 함수를 클래스로 묶기 => Reading 클래스로 함수 옮기기 및 함수 이름 바꾸기

--- Chapter_06/index_9.js
+++ Chapter_06/index_9.js
@@ -8,11 +8,7 @@ const baseCharge = aReading.baseCharge;
 // 클라이언트 2
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = taxableChargeFn(aReading);
-
-function taxableChargeFn(aReading) {
-    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
-}
+const taxableCharge = aReading.taxableCharge;
 
 // 클라이언트 3
 const rawReading = acquireReading();
@@ -46,6 +42,10 @@ class Reading {
 
     get baseCharge() {
         return baseRate(this.month, this.year) * this.quantity;
+    }
+
+    get taxableCharge() {
+        return Math.max(0, this.baseCharge - taxThreshold(this.year));
     }
 }
 
