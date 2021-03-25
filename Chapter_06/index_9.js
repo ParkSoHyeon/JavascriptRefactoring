11da0d4d397585ffb4dcf2d23164df81ffe935a8
ParkSoHyeon <dso753@naver.com>
2021-03-26 00:46:39 +0900

    Chapter_06 예시: 여러 함수를 클래스로 묶기 => 중복된 계산 코드

--- Chapter_06/index_9.js
+++ Chapter_06/index_9.js
@@ -1,8 +1,9 @@
 reading = {customer: 'ivan', quantity: 10, month: 5, year: 2017};
 
 // 클라이언트 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 
 // 클라이언트 2
