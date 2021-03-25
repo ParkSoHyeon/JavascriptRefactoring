9027b7f31d35f734593e0e10f14b5c7841ba762a
ParkSoHyeon <dso753@naver.com>
2021-03-26 01:14:38 +0900

    Chapter_06 예시: 여러 함수를 변환 함수로 묶기 => calculateBaseCharge 함수 옮기기

--- Chapter_06/index_10.js
+++ Chapter_06/index_10.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const assert = require('chai').assert;
 
 reading = {customer: 'ivan', quantity: 10, month: 5, year: 2017};
 
@@ -15,7 +16,7 @@ const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 // 클라이언트 3
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const basicChargeAmount = calculateBaseCharge(aReading);
+const basicChargeAmount = aReading.baseCharge;
 
 function calculateBaseCharge(aReading) {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
@@ -23,7 +24,15 @@ function calculateBaseCharge(aReading) {
 
 function enrichReading(original) {
     const result = _.cloneDeep(original);
+    result.baseCharge = calculateBaseCharge(result);
     return result;
 }
+
+it('check reading unchanged', function () {
+    const baseReading = {customer: 'ivan', quantity: 10, month: 5, year: 2017};
+    const oracle = _.cloneDeep(baseReading);
+    enrichReading(baseReading);
+    assert.deepEqual(baseReading, oracle);
+})
 
 
