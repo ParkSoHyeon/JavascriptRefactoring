2977885b6ee742507853b72bd58030de4d668fea
ParkSoHyeon <dso753@naver.com>
2021-03-30 02:24:50 +0900

    Chapter_07 예시: 간단한 레코드 캡슐화하기 => 변수 캡슐화하기

--- Chapter_07/index_1.js
+++ Chapter_07/index_1.js
@@ -1,5 +1,9 @@
 const organization = {name: '애크미 구스베리', country: 'GB'};
 
 newName = '구스베리'
-result += `<h1>${organization.name}</h1>`;
-organization.name = newName;
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getRawDataOfOrganization().name = newName;
+
+function getRawDataOfOrganization() {
+    return organization;
+}
