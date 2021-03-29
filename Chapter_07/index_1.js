6955f4b634b927a94b00c09858f010cc6376462c
ParkSoHyeon <dso753@naver.com>
2021-03-30 02:29:58 +0900

    Chapter_07 예시: 간단한 레코드 캡슐화하기 => 게터/세터 사용

--- Chapter_07/index_1.js
+++ Chapter_07/index_1.js
@@ -1,8 +1,8 @@
 const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
 
 newName = '구스베리'
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;
-getRawDataOfOrganization().name = newName;
+result += `<h1>${getOrganization().name}</h1>`;
+getOrganization().name = newName;
 
 function getRawDataOfOrganization() {
     return organization._data;
@@ -15,5 +15,13 @@ function getOrganization() {
 class Organization {
     constructor(data) {
         this._data = data;
+    }
+
+    set name(arg) {
+        this._data.name = arg;
+    }
+
+    get name() {
+        return this._data.name;
     }
 }
