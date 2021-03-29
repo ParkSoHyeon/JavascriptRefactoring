377bddf9985d373c6760f8057841c2616eb39350
ParkSoHyeon <dso753@naver.com>
2021-03-30 02:28:17 +0900

    Chapter_07 예시: 간단한 레코드 캡슐화하기 => 레코드 -> 클래스, 새 클래스의 인스턴스를 반환하는 함수 생성

--- Chapter_07/index_1.js
+++ Chapter_07/index_1.js
@@ -1,9 +1,19 @@
-const organization = {name: '애크미 구스베리', country: 'GB'};
+const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
 
 newName = '구스베리'
 result += `<h1>${getRawDataOfOrganization().name}</h1>`;
 getRawDataOfOrganization().name = newName;
 
 function getRawDataOfOrganization() {
+    return organization._data;
+}
+
+function getOrganization() {
     return organization;
+}
+
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
 }
