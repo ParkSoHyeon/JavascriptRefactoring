a7862a0d874eff13a0a162c6006637ced1d6d91f
ParkSoHyeon <dso753@naver.com>
2021-03-31 01:33:14 +0900

    Chapter_07 예시: 컬렉션 캡슐화하기 => 컬렉션 변경자 메서드 생성(add(), remove())

--- Chapter_07/index_2.js
+++ Chapter_07/index_2.js
@@ -15,6 +15,21 @@ class Person {
     set courses(arg) {
         this._courses = arg;
     }
+
+    addCourse(aCourse) {
+        this._courses.push(aCourse);
+    }
+
+    removeCourse(aCourse, fnIfAbsent = () => {throw new RangeError()}) {
+        const index = this._courses.indexOf(aCourse);
+
+        if (index === -1) {
+            fnIfAbsent();
+        }
+        else {
+            this._courses.splice(index, 1);
+        }
+    }
 }
 
 class Course {
