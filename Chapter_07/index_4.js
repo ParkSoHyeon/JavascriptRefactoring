c263d603f6c36aa5305a84f27ba1cb25142fe809
ParkSoHyeon <dso753@naver.com>
2021-04-01 01:29:35 +0900

    Chapter_07 예시: 임시 변수를 질의 함수로 바꾸기 => 변수 인라인하기

--- Chapter_07/index_4.js
+++ Chapter_07/index_4.js
@@ -5,14 +5,13 @@ class Order {
     }
 
     get price() {
-        const basePrice = this.basePrice;
         var discountFactor = 0.98;
 
-        if (basePrice > 1000) {
+        if (this.basePrice > 1000) {
             discountFactor -= 0.03;
         }
 
-        return basePrice * discountFactor;
+        return this.basePrice * discountFactor;
     }
 
     get basePrice() {
