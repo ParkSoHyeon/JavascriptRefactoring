f3f859d96ee14b6c6ba37c5dfbca168cccd30f80
ParkSoHyeon <dso753@naver.com>
2021-04-01 01:44:14 +0900

    Chapter_07 예시: 클래스 추출하기 => 전화번호 관련 동작 별도 클래스로 추출

--- Chapter_07/index_5.js
+++ Chapter_07/index_5.js
@@ -1,4 +1,8 @@
 class Person {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
+
     get name() {
         return this._name;
     }
@@ -7,6 +11,28 @@ class Person {
         this._name = arg;
     }
 
+    get telephoneNumber() {
+        return this._telephoneNumber.telephoneNumber;
+    }
+
+    get officeAreaCode() {
+        return this._telephoneNumber.officeAreaCode;
+    }
+
+    set officeAreaCode(arg) {
+        this._telephoneNumber.officeAreaCode = arg;
+    }
+
+    get officeNumber() {
+        return this._telephoneNumber.officeNumber;
+    }
+
+    set officeNumber(arg) {
+        this._telephoneNumber.officeNumber = arg;
+    }
+}
+
+class TelephoneNumber {
     get telephoneNumber() {
         return `(${this.officeAreaCode}) ${this.officeNumber}`;
     }
