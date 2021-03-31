3308646ffe29f2e2824014407af524d10b67005d
ParkSoHyeon <dso753@naver.com>
2021-04-01 01:54:44 +0900

    Chapter_07 예시 기본 코드: 클래스 인라인하기

---
+++ Chapter_07/index_6.js
@@ -0,0 +1,39 @@
+class TrackingInformation {
+    get shippingCompany() {
+        return this._shippingCompany;
+    }
+
+    set shippingCompany(arg) {
+        this._shippingCompany = arg;
+    }
+
+    get trackingNumber() {
+        return this._trackingNumber;
+    }
+
+    set trackingNumber(arg) {
+        this._trackingNumber = arg
+    }
+
+    get display() {
+        return `${this.shippingCompany}: ${this.trackingNumber}`;
+    }
+}
+
+class Shipment {
+    get trackingInfo() {
+        return this._trackingInformation.display;
+    }
+
+    get trackingInformation() {
+        return this._trackingInformation;
+    }
+
+    set trackingInformation(arg) {
+        this._trackingInformation = arg;
+    }
+}
+
+// 클라이언트
+const aShipment = new Shipment();
+aShipment.trackingInformation.shippingCompany = request.vendor;
